cd1b517ebfa83eba84e5dfa1934e54c59ac6f48c
Jose Daniel <josedanq100@gmail.com>
2018-12-18 12:22:48 -0400

    Delete test code

--- main.c
+++ main.c
@@ -135,49 +135,7 @@ int main(){
         character = turns->chars[turnCounter];
         // waitForKeyPress();
     } while (isInGame);
-
-
-    if (isFree(map, createCord('A', 1))) printf("\nEsta casilla esta desocupada\n");
-    else printf("\nEsta casilla esta ocupada\n");
-
-    addItemToInventory (turns->chars[0], createEnergyPotion());
-    addItemToInventory (turns->chars[0], createHealthPotion());
-
-    Item *item = getTopItemFromInventory(turns->chars[0]->items);
-    map->cols[0]->lands[0]->character = turns->chars[0];
-    map->cols[1]->lands[1]->character = turns->chars[1];
-
-    land = map->cols[0]->lands[0];
-
-    if (isFree(map, createCord('A', 1))) printf("\nEsta casilla esta desocupada\n");
-    else printf("\nEsta casilla esta ocupada\n");
-
-
-    // testing effect
-    (*(*turns->chars)->items)->item->effect(land);
-
-    switch(confirm("dejar este item en el terreno")) {
-
-        case 1:
-            dropItemToLand(turns->chars[0], land);
-            break;
-
-        case 2:
-            printf("Accion cancelada");
-            break;
-    }
-
-    seeLand(map, createCord('A', 1));
-
-    turns->chars[0]->aP = 20;
-
-    useSkill(turns->chars[0], map, createCord('B', 2), turns->chars[0]->skills[0]->skill);
-
-    // moveCharacterToCords(map, getCharacterCords(map, turns->chars[0]), createCord('B', 1));
-
-
     
-
     #if SO == 1
         system("PAUSE");
     #endif
